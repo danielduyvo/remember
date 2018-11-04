b441d1e2e4a27026a16a7fb1ccb9f5d77abbe7c5
danielduyvo <danielduyvo@yahoo.com>
2018-11-04 10:30:21 -0800

    Created a GitHub page for the application

--- config.edit/encrypt.js
+++
@@ -1,7 +0,0 @@
-const crypto = require('crypto');
-
-function crypt(string) {
-    return string; //edit this file to encrypt passwords
-}
-
-module.exports = crypt;
--- config.edit/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = { //fill this out with MySQL database credentials
-    host: '',
-    user: '',
-    password: '',
-    database: '',
-}
